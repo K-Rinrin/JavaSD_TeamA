5c4a89335b550ef35f9ffc0199aabf865d743b0a
k_ishi <k_ishi@LAPTOP-MLKKGP54>
2025-06-11 12:36:02 +0900

    a

--- src/acoounts/LogoutController.java
+++ src/acoounts/LogoutController.java
@@ -1,20 +1,18 @@
 package acoounts;
 
-import javax.servlet.annotation.WebServlet;
-
 //ログイン機能
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import tool.CommonServlet;
-@WebServlet(urlPatterns = { "/accounts/login" })
-public class LoginController extends CommonServlet {
+
+public class LogoutController extends CommonServlet {
 
 	@Override
 	protected void get(HttpServletRequest req, HttpServletResponse resp) throws Exception {
 		// TODO 自動生成されたメソッド・スタブ
-		req.getRequestDispatcher("LOGI001.jsp").forward(req, resp);
+
 	}
 
 	@Override
