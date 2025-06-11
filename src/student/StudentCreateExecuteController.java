1e1ba8c1726c148cb862eeab6671b2c19dbf869c
k_hirama <k_hirama@LAPTOP-1D7J63SQ>
2025-06-11 11:47:12 +0900

    ｐ

---
+++ src/student/StudentCreateExecuteController.java
@@ -0,0 +1,23 @@
+package student;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import tool.CommonServlet;
+
+public class StudentCreateExecuteController extends CommonServlet {
+
+	//学生登録
+	@Override
+	protected void get(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	protected void post(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
