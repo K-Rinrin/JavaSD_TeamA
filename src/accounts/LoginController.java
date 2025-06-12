0b05047aebdccd7e2063ff0e518df8596474d103
k_ishi <k_ishi@LAPTOP-MLKKGP54>
2025-06-12 11:01:14 +0900

    login

--- src/accounts/LoginController.java
+++ src/accounts/LoginController.java
@@ -9,34 +9,20 @@ import javax.servlet.http.HttpServletResponse;
 import tool.CommonServlet;
 
 // このアノテーションにより、/accounts/login へのリクエストがこのサーブレットにマッピングされます。
-@WebServlet(urlPatterns = { "/accounts/login" })
+@WebServlet(urlPatterns = { "/accounts/LOGI001" })
 public class LoginController extends CommonServlet {
 
-	/**
-	 * HTTP GET リクエストを処理します。
-	 * ログイン画面 (LOGI001.jsp) を表示します。
-	 *
-	 * @param req  HttpServletRequest オブジェクト
-	 * @param resp HttpServletResponse オブジェクト
-	 * @throws Exception 処理中にエラーが発生した場合
-	 */
 	@Override
 	protected void get(HttpServletRequest req, HttpServletResponse resp) throws Exception {
 		// LOGI001.jsp にフォワードしてログイン画面を表示
 		req.getRequestDispatcher("LOGI001.jsp").forward(req, resp);
+
 	}
 
-	/**
-	 * HTTP POST リクエストを処理します。
-	 * 通常、ログインフォームの送信時に呼び出され、ログイン実行処理にリダイレクトします。
-	 *
-	 * @param req  HttpServletRequest オブジェクト
-	 * @param resp HttpServletResponse オブジェクト
-	 * @throws Exception 処理中にエラーが発生した場合
-	 */
 	@Override
 	protected void post(HttpServletRequest req, HttpServletResponse resp) throws Exception {
 		// ログイン実行処理を行う /accounts/loginexecute へリダイレクト
-		resp.sendRedirect(req.getContextPath() + "/accounts/loginExecute");
+//		resp.sendRedirect(req.getContextPath() + "/accounts/loginExecute");
+
 	}
 }
--- src/accounts/LoginExecuteController.java
+++ src/accounts/LoginExecuteController.java
@@ -8,37 +8,17 @@ import javax.servlet.http.HttpSession;
 
 import bean.Teacher;
 import dao.TeacherDao;
-import tool.CommonServlet; 
+import tool.CommonServlet;
 
 // このアノテーションにより、/accounts/loginExecute へのリクエストがこのサーブレットにマッピングされます。
-@WebServlet(urlPatterns = { "/accounts/loginExecute" })
+@WebServlet(urlPatterns = { "/accounts/LOGI001" })
 public class LoginExecuteController extends CommonServlet {
 
-	/**
-	 * HTTP GET リクエストを処理します。
-	 * このサーブレットではGETリクエストは想定されていませんが、必要に応じて実装できます。
-	 * 現在は空の実装です。
-	 *
-	 * @param req  HttpServletRequest オブジェクト
-	 * @param resp HttpServletResponse オブジェクト
-	 * @throws Exception 処理中にエラーが発生した場合
-	 */
 	@Override
 	protected void get(HttpServletRequest req, HttpServletResponse resp) throws Exception {
-		// GETリクエスト時の処理は現在ありません。
 		// TODO: 必要に応じてGETリクエストの処理を実装する
 	}
 
-	/**
-	 * HTTP POST リクエストを処理します。
-	 * ログインフォームから送信されたIDとパスワードを使用して認証を行います。
-	 * 認証成功時にはセッションにユーザー情報を保存し、メイン画面へリダイレクトします。
-	 * 認証失敗時にはエラーメッセージを設定し、ログイン画面にフォワードします。
-	 *
-	 * @param req  HttpServletRequest オブジェクト
-	 * @param resp HttpServletResponse オブジェクト
-	 * @throws Exception 処理中にエラーが発生した場合
-	 */
 	@Override
 	protected void post(HttpServletRequest req, HttpServletResponse resp) throws Exception {
 		// 現在のセッションを取得します。セッションが存在しない場合は新しく作成されます。
@@ -52,25 +32,30 @@ public class LoginExecuteController extends CommonServlet {
 		TeacherDao dao = new TeacherDao();
 		Teacher teacher = dao.login(id, password); // ログインIDとパスワードで教員情報を検索します。
 
-		// 取得した教員情報がnullでない場合（認証成功）
-		if (teacher != null) {
-			// 認証成功時：取得した教員情報をセッションに保存します。
-			// "session_user" という名前でセッションアトリビュートとして設定します。
-			session.setAttribute("session_user", teacher);
+		try {
 
-			// ログイン成功後、アプリケーションのコンテキストパスに "/menu" を付加したURLへリダイレクトします。
-			// これにより、ブラウザは指定されたURLに再度アクセスします。
-			resp.sendRedirect(req.getContextPath() + "/menu");
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+			// 取得した教員情報がnullでない場合（認証成功）
+			if (teacher != null) {
+				// 認証成功時：取得した教員情報をセッションに保存します。
+				// "session_user" という名前でセッションアトリビュートとして設定します。
+				session.setAttribute("session_user", teacher);
 
-		} else {
-			// 認証失敗時：入力されたログインIDをリクエスト属性にセットし、ログイン画面で再表示できるようにします。
-			req.setAttribute("id", id);
+				// ログイン成功後、アプリケーションのコンテキストパスに "/menu" を付加したURLへリダイレクトします。
+				// これにより、ブラウザは指定されたURLに再度アクセスします。
+				resp.sendRedirect(req.getContextPath() + "/menu");
+
+			} else {
+				// 認証失敗時：入力されたログインIDをリクエスト属性にセットし、ログイン画面で再表示できるようにします。
+				req.setAttribute("id", id);
+
+				// ログイン画面 (LOGI001.jsp) にフォワードし、ユーザーに入力ミスを修正して再試行を促します。
+				req.getRequestDispatcher("LOGI001.jsp").forward(req, resp);
 
 			// エラーメッセージをリクエスト属性に追加する場合のコメントアウトされたコードです。
 			// req.setAttribute("errorMessage", "ログイン名またはパスワードが違います");
-
-			// ログイン画面 (LOGI001.jsp) にフォワードし、ユーザーに入力ミスを修正して再試行を促します。
-			req.getRequestDispatcher("LOGI001.jsp").forward(req, resp);
 		}
 	}
 }
