e06b39f913560cd593a303843682e55cbf7b4c5f
k_ishi <k_ishi@LAPTOP-MLKKGP54>
2025-06-12 12:27:21 +0900

    menu

--- src/main/MenuController.java
+++ src/main/MenuController.java
@@ -2,17 +2,19 @@ package main;
 
 //メインメニュー画面
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import tool.CommonServlet;
+@WebServlet(urlPatterns = { "/main/MMNU001" })
 
 public class MenuController extends CommonServlet {
 
 	@Override
 	protected void get(HttpServletRequest req, HttpServletResponse resp) throws Exception {
 		// TODO 自動生成されたメソッド・スタブ
-        req.getRequestDispatcher("MMUN.jsp").forward(req, resp);
+        req.getRequestDispatcher("MMNU001.jsp").forward(req, resp);
 	}
 
 	@Override
