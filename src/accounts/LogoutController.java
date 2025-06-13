20df696741fc7f6f0dafe358a918e073ccc7eb05
k_ishi <k_ishi@LAPTOP-MLKKGP54>
2025-06-13 12:32:15 +0900

    a

--- src/accounts/LogoutController.java
+++ src/accounts/LogoutController.java
@@ -5,6 +5,7 @@ package accounts;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import tool.CommonServlet;
 @WebServlet(urlPatterns = { "/main/accounts/LOGO001" })
@@ -14,6 +15,11 @@ public class LogoutController extends CommonServlet {
 	@Override
 	protected void get(HttpServletRequest req, HttpServletResponse resp) throws Exception {
 		// TODO 自動生成されたメソッド・スタブ
+		// セッションを取得（存在しない場合は新規作成される）
+        HttpSession session = req.getSession();
+
+        // セッションからユーザー情報と管理者フラグを削除
+        session.removeAttribute("name"); // ログインユーザー情報の削除
         req.getRequestDispatcher("LOGO001.jsp").forward(req, resp);
 	}
 
