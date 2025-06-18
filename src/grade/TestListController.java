0290597646688da4331fa7584f829eed5f7e2eff
k_hirama <k_hirama@LAPTOP-1D7J63SQ>
2025-06-18 10:33:10 +0900

    l

--- src/grade/TestListController.java
+++ src/grade/TestListController.java
@@ -7,13 +7,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import tool.CommonServlet;
-@WebServlet(urlPatterns = { "/main/accounts/GRMR001" })
 
+@WebServlet(urlPatterns = { "/main/accounts/testlist" })
 public class TestListController extends CommonServlet {
 
 	@Override
 	protected void get(HttpServletRequest req, HttpServletResponse resp) throws Exception {
 		// TODO 自動生成されたメソッド・スタブ
+
+		//年度　クラス　科目　回数
+		String entyear = req.getParameter("ent_year");
+		String classnum = req.getParameter("class_num");
+		String subject = req.getParameter("subject");
 
 	}
 
