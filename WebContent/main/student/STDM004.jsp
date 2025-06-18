fd6a0be9d64f5bb005778aeae39b9c1d3673bde1
k_hirama <k_hirama@LAPTOP-1D7J63SQ>
2025-06-18 12:41:31 +0900

    ｎ

--- WebContent/main/student/STDM004.jsp
+++ WebContent/main/student/STDM004.jsp
@@ -26,13 +26,13 @@
 					<%-- 入学年度の表示（読み取り） --%>
 					<div class="mb-3">
 						<label class="form-label">入学年度</label>
-						<input type="text" value="${ent_year}" readonly class="form-control">
+						<input type="text" value="${student.entYear}" readonly class="form-control">
 					</div>
 
 					<%-- 学生番号の表示（読み取り） --%>
 					<div class="mb-3">
 						<label class="form-label">学生番号</label>
-						<input type="text" value="${no}" readonly class="form-control">
+						<input type="text" value="${student.no}" readonly class="form-control">
 					</div>
 
 					<%-- 氏名の入力 --%>
--- src/student/StudentUpdateController.java
+++ src/student/StudentUpdateController.java
@@ -19,10 +19,25 @@ public class StudentUpdateController extends CommonServlet {
 
 		//更新する学生の学生番号をもらう
 		String no = req.getParameter("no");
-		StudentDAO dao = new StudentDAO();
-		Student stu = dao.getStudentByNo(no);
 
-		req.setAttribute("stu", stu);
+		//学生情報をもらう
+		StudentDAO dao = new StudentDAO();
+		Student student = dao.getStudentByNo(no);
+
+		//確認
+		if (student != null) {
+		    System.out.println("【確認】学生情報取得成功:");
+		    System.out.println("学生番号: " + student.getNo());
+		    System.out.println("氏名: " + student.getName());
+		    System.out.println("入学年度: " + student.getEntYear());
+		    System.out.println("クラス: " + student.getClassNum());
+		    System.out.println("在学中: " + student.isAttend());
+		} else {
+		    System.out.println("【警告】学生情報が null です。学生番号: " + no);
+		}
+
+		//学生情報をわたす
+		req.setAttribute("student", student);
 
 		req.getRequestDispatcher("/main/student/STDM004.jsp").forward(req, resp);
 
